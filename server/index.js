ded64e70f91b52fa6700e70330211b1b255c9938
Nathaniel Shirts <nshirts7@gmail.com>
2021-03-29 21:15:39 -0600

    added a param and query to the get requests

--- server/index.js
+++ server/index.js
@@ -3,8 +3,20 @@ const port = 3001;
 const app = express();
 const data = require('./MOCK_DATA.json');
 
-app.get('/api/animals', (_, res) => {
-  res.status(200).json(data);
+app.get('/api/animals', (req, res) => {
+  let { animal } = req.query;
+  let animalData = data.filter(
+    (val) => val.animal.toLowerCase() === animal.toLowerCase()
+  );
+  if (animalData.length > 0) {
+    res.status(200).json(animalData);
+  } else res.status(200).json(data);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+  let { id } = req.params;
+  let idData = data.filter((val) => val.id == id);
+  res.status(200).json(idData);
 });
 
 app.listen(port, console.log(`Server is running on port ${port}`));
